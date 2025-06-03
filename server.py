4ce0a6e23f1a21e9faf62c7b3f60874382b72552
rvuyyuru2 <rajasekhar@instaread.co>
2025-06-03 17:55:41 +0530

    feat(middleware): add PUT and DELETE methods to CORS configuration
    Enable support for PUT and DELETE HTTP methods in the CORS middleware to allow more RESTful API operations

--- server.py
+++ server.py
@@ -188,7 +188,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_methods=["GET", "POST", "OPTIONS","PUT","DELETE"],
     allow_headers=["*"],
 )
 
